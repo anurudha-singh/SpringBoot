a1b281ba2546156e98ec9a3c4771e8c21e6ade51
Anurudha_Singh <singhanurudh570@gmail.com>
2024-08-05 14:00:09 +0530

    Birthday greet added

--- demo/src/main/java/controller/HelloController.java
+++ demo/src/main/java/controller/HelloController.java
@@ -10,4 +10,11 @@ public class HelloController {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {//If we had defined the "name" query parameter then it will be assigned to the name variable of hello() method otherwise the default value will be set as "world"
         return String.format("Hello %s!", name);
     }
+
+    @GetMapping("/birthday")
+
+    public String birthdayGreet(@RequestParam(value = "name", defaultValue = "World") String name){
+        return "Happy birthday :: "+name;
+    }
 }
+
