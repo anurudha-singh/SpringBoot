dcba078c2ddf73fa877f9f9e5ef3a449e351dcea
Anurudha_Singh <singhanurudh570@gmail.com>
2024-08-10 09:46:58 +0530

    Saving code file with Whitelabel error

--- demo/src/main/java/controller/MyController.java
+++ demo/src/main/java/controller/MyController.java
@@ -1,56 +1,56 @@
 package controller;
 
-//import entities.Course;
+import entities.Course;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//import services.CourseService;
+import services.CourseService;
 
 import java.util.List;
 
 @RestController
 public class MyController {
-    private static final Logger log = LoggerFactory.getLogger(MyController.class);
+//    private static final Logger log = LoggerFactory.getLogger(MyController.class);
     //Some sort of Run time polymorphism
-//    @Autowired  //Spring boot will create the object of CourseServiceImplementation class and inject it into the interface reference, this is known as dependency injection
-//    private CourseService courseService;
+    @Autowired  //Spring boot will create the object of CourseServiceImplementation class and inject it into the interface reference, this is known as dependency injection
+    private CourseService courseService;
 
     @GetMapping("/")
     String welcome(){
         System.out.println("at 222");
         return "Welcome to Spring Boot Rest API class";
     }
-//    @GetMapping("/courses")
-//    List<Course> getCourses() {
-//        return this.courseService.getCourses();
-//    }
-//    @GetMapping("/course/{courseID}")
-//    public Course getCourse(@PathVariable String courseID ){
-//        return this.courseService.getCourse(Integer.parseInt(courseID));
-//    }
-//    @PostMapping("/addCourse")
-//    public Course addCourse(@RequestBody Course course){
-////        System.out.println("Course added!");
-//       return courseService.addCourse(course);
-//
-//    }
-//    @PutMapping("/update/course/{courseID}")
-//    public String updateCourseDetails(@RequestBody Course courseDetails,@PathVariable String courseID){
-//        try {
-//            courseService.updateCourseDetails(courseDetails,Integer.parseInt(courseID));
-//            return String.valueOf(new ResponseEntity<>(HttpStatus.OK));
-//        } catch (NumberFormatException e) {
-//            return String.valueOf(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    @DeleteMapping("/delete/course/{courseID}")
-//    public String deleteCourseDetails(@PathVariable String courseID){
-//        return courseService.deleteCourseDetails(Integer.parseInt(courseID));
-//    }
+    @GetMapping("/courses")
+    List<Course> getCourses() {
+        return this.courseService.getCourses();
+    }
+    @GetMapping("/course/{courseID}")
+    public Course getCourse(@PathVariable String courseID ){
+        return this.courseService.getCourse(Integer.parseInt(courseID));
+    }
+    @PostMapping("/addCourse")
+    public Course addCourse(@RequestBody Course course){
+//        System.out.println("Course added!");
+       return courseService.addCourse(course);
+
+    }
+    @PutMapping("/update/course/{courseID}")
+    public String updateCourseDetails(@RequestBody Course courseDetails,@PathVariable String courseID){
+        try {
+            courseService.updateCourseDetails(courseDetails,Integer.parseInt(courseID));
+            return String.valueOf(new ResponseEntity<>(HttpStatus.OK));
+        } catch (NumberFormatException e) {
+            return String.valueOf(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DeleteMapping("/delete/course/{courseID}")
+    public String deleteCourseDetails(@PathVariable String courseID){
+        return courseService.deleteCourseDetails(Integer.parseInt(courseID));
+    }
 }
--- demo/src/main/java/services/CourseServiceImplementation.java
+++ demo/src/main/java/services/CourseServiceImplementation.java
@@ -1,76 +1,76 @@
-//package services;
-//import dao.CourseDao;
-//import entities.Course;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import java.util.ArrayList;
-//import java.util.List;
+package services;
+import dao.CourseDao;
+import entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CourseServiceImplementation implements CourseService{
+
+   @Autowired
+    private CourseDao courseDao;
+//    List<Course> list;
+
+    public CourseServiceImplementation() {
+//        list=new ArrayList<>();
+//        list.add(new Course(1,"Java Course","Basics of Java"));
+//        list.add(new Course(2,"Spirng boot","Basics of Rest APIs"));
+    }
+
+    @Override
+    public List<Course> getCourses() {
+        return courseDao.findAll();
+    }
+
+    @Override
+    public Course getCourse(int courseId) {
+        return courseDao.getReferenceById((long) courseId);
+//        Course requestedCourse = null;
+//        for (Course course : list) {
+//            if (course.getID() == courseId) {
+//                requestedCourse = course;
+//                break;
+//            }
 //
-//@Service
-//public class CourseServiceImplementation implements CourseService{
-//
-//   @Autowired
-//    private CourseDao courseDao;
-////    List<Course> list;
-//
-//    public CourseServiceImplementation() {
-////        list=new ArrayList<>();
-////        list.add(new Course(1,"Java Course","Basics of Java"));
-////        list.add(new Course(2,"Spirng boot","Basics of Rest APIs"));
-//    }
-//
-//    @Override
-//    public List<Course> getCourses() {
-//        return courseDao.findAll();
-//    }
-//
-//    @Override
-//    public Course getCourse(int courseId) {
-//        return courseDao.getReferenceById((long) courseId);
-////        Course requestedCourse = null;
-////        for (Course course : list) {
-////            if (course.getID() == courseId) {
-////                requestedCourse = course;
-////                break;
-////            }
-////
-////        }
-////        return requestedCourse;
-//    }
-//
-//    @Override
-//    public Course addCourse(Course course){
-//        return courseDao.save(course);
-////        list.add(course);
-////        return "Course was added successfully";
-//    }
-//
-//    @Override
-//    public void updateCourseDetails(Course course, int courseID) {
-//        courseDao.save(course);
-//        //        for(Course currentCourse : list){
-////            if(currentCourse.getID()==courseID){
-////                currentCourse.setTitle(course.getTitle());
-////                currentCourse.setDescription(course.getDescription());
-////                break;
-////            }
-////            return "Course details were updated successfully!";
-////        }
-////        return  new Exception().toString();
-//
-//    }
-//
-//    @Override
-//    public String deleteCourseDetails(int courseID) {
-////        for(Course currentCourse : list){
-////            if(currentCourse.getID()==courseID){
-////                list.remove(currentCourse);
-////                break;
-////            }
-////        }
-////        return "Course was deleted successfully!";
-//        Course entity=courseDao.getOne((long) courseID);
-//        courseDao.delete(entity);
+//        }
+//        return requestedCourse;
+    }
+
+    @Override
+    public Course addCourse(Course course){
+        return courseDao.save(course);
+//        list.add(course);
+//        return "Course was added successfully";
+    }
+
+    @Override
+    public void updateCourseDetails(Course course, int courseID) {
+        courseDao.save(course);
+        //        for(Course currentCourse : list){
+//            if(currentCourse.getID()==courseID){
+//                currentCourse.setTitle(course.getTitle());
+//                currentCourse.setDescription(course.getDescription());
+//                break;
+//            }
+//            return "Course details were updated successfully!";
+//        }
+//        return  new Exception().toString();
+
+    }
+
+    @Override
+    public String deleteCourseDetails(int courseID) {
+//        for(Course currentCourse : list){
+//            if(currentCourse.getID()==courseID){
+//                list.remove(currentCourse);
+//                break;
+//            }
+//        }
 //        return "Course was deleted successfully!";
-//    }
-//}
+        Course entity=courseDao.getOne((long) courseID);
+        courseDao.delete(entity);
+        return "Course was deleted successfully!";
+    }
+}
